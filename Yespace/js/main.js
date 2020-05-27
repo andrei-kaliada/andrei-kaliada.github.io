9f39c3233d9ae778e91432f3bbde1649e2fc08c5
Andrei Kaliada <49281134+andrei-kaliada@users.noreply.github.com>
2020-05-27 19:03:54 +0200

    Add files via upload

--- Yespace/js/main.js
+++ Yespace/js/main.js
@@ -1,5 +1,6 @@
 const dropdownBtn = document.querySelector('.dropdown');
 const dropdownMenu = document.querySelector('.dropdownMenu');
+const cardCent = document.querySelector('.card-centurion');
 
 function showDropdownMenu(){
     dropdownMenu.classList.toggle('dropdownMenu-active')
--- Yespace/js/slickSlider.js
+++ Yespace/js/slickSlider.js
@@ -2,7 +2,7 @@
 $('.center').slick({
     centerMode: false,
     centerPadding: '0px',
-    slidesToShow: 4.5,
+    slidesToShow: 4,
     infinite:false,
     responsive: [
       {
@@ -40,4 +40,49 @@ $('.center').slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 3
+});
+
+
+$('.center').on('afterChange', function(){
+  console.log($('.center').slick('slickCurrentSlide'));
+  let currentSlide = $('.center').slick('slickCurrentSlide');
+if(currentSlide==0)
+{
+   $('.slick-prev').hide();
+   $('.slick-next').show();
+   $('.card-centurion').css('opacity',0.5);
+   $('.card-gold').css('opacity',1);
+}
+else if(currentSlide==4)
+{
+$('.slick-next').hide();
+$('.slick-prev').show();
+}
+
+if(currentSlide>0 && currentSlide<4)
+{
+   $('.slick-prev').show();
+   $('.slick-next').hide();
+   $('.card-gold').css('opacity',0.5);
+   $('.card-centurion').css('opacity',1);
+}
+
+});
+
+$(document).ready(function(){
+let currentSlide = $('.center').slick('slickCurrentSlide');
+if(currentSlide==0)
+{
+   $('.slick-prev').hide();
+   $('.card-centurion').css('opacity',0.5);
+   ('.card-gold').css('opacity',1);
+  
+}
+else if(currentSlide==3)
+{
+$('.slick-next').hide();
+$('.card-gold').css('opacity',0.5);
+$('.card-centurion').css('opacity',1);
+}
+
 });
