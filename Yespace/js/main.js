83cdad836ba0d558e75b933474d98221e2b89243
Andrei Kaliada <49281134+andrei-kaliada@users.noreply.github.com>
2020-06-18 19:25:09 +0200

    Add files via upload

--- Yespace/js/main.js
+++ Yespace/js/main.js
@@ -2,6 +2,8 @@ const dropdownBtn = document.querySelector('.dropdown');
 const dropdownMenu = document.querySelector('.dropdownMenu');
 const cardCent = document.querySelector('.card-centurion');
 const messageBtn = document.querySelectorAll('.btn_value');
+const popup = document.querySelector('.mobile-header__burger');
+const wrappPopUp = document.querySelector('.popup-mobile');
 
 function setValueBtn(event){
    let value = event.target.value;
@@ -20,3 +22,44 @@ messageBtn.forEach( element => {
     element.addEventListener('input', setValueBtn);
 })
 
+function showPopUp(){
+   
+    popup.classList.toggle('activePopUp');
+    wrappPopUp.classList.toggle('showWrappPopUp');
+    document.body.classList.toggle('fixed');
+}
+
+popup.addEventListener('click', showPopUp);
+
+
+let form = document.querySelectorAll('.my-form');
+let msgFormText = document.querySelectorAll('.message-form__text');
+
+form.forEach(el => el.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+
+  let formData = {
+    name: document.querySelector('input[name="email"]').value,
+  };
+
+  let request = new XMLHttpRequest();
+
+  request.addEventListener('load', function() {
+    // В этой части кода можно обрабатывать ответ от сервера
+    console.log(request.response);
+    form.forEach( el => el.innerHTML = `
+      <div class="sendForm"> 
+        <p class="sendForm__text">Спасибо отправлено!</p>
+      </div>
+    `
+    );
+    msgFormText.forEach( el => {
+      el.innerHTML="Ты молодец!";
+      el.style.paddingLeft = 0;
+    })
+  });
+
+  request.open('POST', '/send.php', true);
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  request.send('name=' + encodeURIComponent(formData.name) );
+}));
