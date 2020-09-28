bf356d3b09761a3a29783ee430ec8d8e62dd2386
Andrei Kaliada <49281134+andrei-kaliada@users.noreply.github.com>
2020-09-28 22:20:24 +0200

    Add files via upload

---
+++ Social-Network/precache-manifest.2837ede9173bf5c401f7c3f0e3e0253d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7613cb28d84cb219871c10987eff8f2d",
+    "url": "/Social-media/index.html"
+  },
+  {
+    "revision": "f9da3dd4bd97380dd8e0",
+    "url": "/Social-media/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b340707f9afe770407be",
+    "url": "/Social-media/static/css/3.a058b33a.chunk.css"
+  },
+  {
+    "revision": "0b385b72cef01ab7213e",
+    "url": "/Social-media/static/css/4.afbb0d15.chunk.css"
+  },
+  {
+    "revision": "f46d31dd6b581c6ab334",
+    "url": "/Social-media/static/css/5.83965fe2.chunk.css"
+  },
+  {
+    "revision": "b40fa8625081149bef73",
+    "url": "/Social-media/static/css/main.0b65189a.chunk.css"
+  },
+  {
+    "revision": "f9da3dd4bd97380dd8e0",
+    "url": "/Social-media/static/js/2.15a0418e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Social-media/static/js/2.15a0418e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b340707f9afe770407be",
+    "url": "/Social-media/static/js/3.44ad4b23.chunk.js"
+  },
+  {
+    "revision": "0b385b72cef01ab7213e",
+    "url": "/Social-media/static/js/4.159b6b2a.chunk.js"
+  },
+  {
+    "revision": "f46d31dd6b581c6ab334",
+    "url": "/Social-media/static/js/5.c51b8eae.chunk.js"
+  },
+  {
+    "revision": "b42fae18fcc355ea394c",
+    "url": "/Social-media/static/js/6.5d76ed5b.chunk.js"
+  },
+  {
+    "revision": "b40fa8625081149bef73",
+    "url": "/Social-media/static/js/main.a172c107.chunk.js"
+  },
+  {
+    "revision": "41cfc93427fe7f349a4d",
+    "url": "/Social-media/static/js/runtime-main.e4f97576.js"
+  }
+]);
